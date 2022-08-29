a2c6e37298e00f1ce3e7b5c01564eef00c35de80
Sourav Paul <souravpaul.mailme@gmail.com>
2022-08-30 01:20:28 +0600

    get  data with API

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # weather_application
+## 4 features of this application
+### 1. Current Weather Show
+### 2. Forecast Weather Show
+### 3. Temperature Convert degree to celsius
+### 4. Location Searching 
--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_application/provider/weather_provider.dart';
+import 'package:weather_application/utils/location_utils.dart';
 
 class WeatherPage extends StatefulWidget {
   static const String routeName = '/weather-page';
@@ -9,6 +12,24 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  late WeatherProvider provider;
+  bool isFirst = true;
+  @override
+  void didChangeDependencies() {
+    if(isFirst){
+      provider = Provider.of<WeatherProvider>(context);
+      _getData();
+      isFirst = false;
+    }
+    super.didChangeDependencies();
+  }
+
+  _getData() {
+    determinePosition().then((position) {
+      provider.setNewLocation(position.latitude, position.longitude);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
